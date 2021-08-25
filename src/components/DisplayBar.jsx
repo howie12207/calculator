e1eadf9bc8e803c4f82e8486a0ddb2a6ca418c1c
Howie <howie12207@gmail.com>
2021-08-25 09:13:16 +0800

    feat: add thousand format

--- src/components/DisplayBar.jsx
+++ src/components/DisplayBar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import { toThousand } from "./operating";
+
 const DisplayBar = ({ displayValue }) => {
   return (
     <div className="bg-darkBlue-900 rounded-md">
-      <p className="p-4 my-4 text-xl text-right text-white">{displayValue}</p>
+      <p className="p-4 my-4 text-xl text-right text-white">
+        {toThousand(displayValue)}
+      </p>
     </div>
   );
 };
--- src/components/operating.jsx
+++ src/components/operating.jsx
@@ -2,6 +2,12 @@ const toDecimalTwo = (value) => {
   return Math.round(value * 10000000000000) / 10000000000000 || 0;
 };
 
+export const toThousand = (value) => {
+  const parts = value.toString().split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return parts.join(".");
+};
+
 export const plus = (prev, next) => {
   return toDecimalTwo(Number(prev) + Number(next));
 };
